5ed151409f0238402fea439c71f5bc25c2061ef4
diegoduqqe@gmail.com <47634821+dukej10@users.noreply.github.com>
2022-04-13 18:58:46 -0500

    mostrar datos como lista

--- algoritmosApp/serializers.py
+++ algoritmosApp/serializers.py
@@ -1,3 +1,4 @@
+from lib2to3.pytree import Node
 from rest_framework import serializers
 from algoritmosApp.models import Departments,Graphs
 
@@ -7,6 +8,8 @@ class DepartmentSerializer(serializers.ModelSerializer):
 		fields = ("departamentId","departamentName")
 
 class GrafoSerializer(serializers.ModelSerializer):
+	nodes = serializers.ListField(child=serializers.DictField())
+	links = serializers.ListField(child=serializers.DictField())
 	class Meta:
 		model = Graphs
 		fields = ("grafoId","grafoName","nodes","links")
